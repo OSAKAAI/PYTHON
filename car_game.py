254869bcd4c0bf5c1ab679ca305f31f046f06af6
OSAKAAI <156836871+OSAKAAI@users.noreply.github.com>
2024-01-30 12:04:59 +0530

    Add files via upload

---
+++ car_game.py
@@ -0,0 +1,10 @@
+car = input("Enter the condition\n")
+
+if car == "stop":
+    print("Stopping the car")
+elif car == "start":
+    print("Car is starting")
+elif car == "quit":
+    print("Quitting...")
+else:
+    print("Exit")
